fc123f8f67f041d6b117d1132c2dbc38343ea648
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2023-09-03 18:22:29 +0000

    upfate configs

---
+++ fpl/__init__.py
@@ -0,0 +1,9 @@
+#noqa
+from . import data_analysis
+from .data_analysis import *
+from . import data_loader
+from .data_loader import *
+from . import prediction
+from .prediction import *
+from . import visualization
+from .visualization import *
