bab9ff41575c7357bcc9d0d9bf6bf6026c1b9a84
hirwa <hirwanshutiflx@gmail.com>
2024-02-10 23:09:55 +0530

    initial loading the data

--- fpl/data_loader/data_download.py
+++ fpl/data_loader/data_download.py
@@ -3,8 +3,36 @@
 import os
 import requests
 import json
+import csv
+
+# global variables
+FPL_DATA_URL = 'https://fantasy.premierleague.com/api/bootstrap-static/'
+
+def download_data(url, file_path):
+    """
+    Downloads data from the FPL API and saves .csv file to the specified file path
+    """
+    response = requests.get(url)
+    with open(file_path, 'w') as f:
+        f.write(response.text)
+
+def convert_json_to_csv(json_file, csv_file):
+    """
+    Converts a .json file to a .csv file
+    """
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+        players_data_keys = data['elements'][0].keys()
+        with open(csv_file, 'w', newline='') as f:
+            writer = csv.DictWriter(f, players_data_keys)
+            writer.writeheader()
+            writer.writerows(data['elements'])
 
 
-def add(x, y):
-    return x + y
-
+if __name__ == '__main__':
+    # download data
+    data_path = os.path.join(os.path.dirname(__file__), 'data')
+    csv_file = os.path.join(data_path, 'fpl_data.csv')
+    json_file = os.path.join(data_path, 'fpl_data.json')
+    convert_json_to_csv(json_file, csv_file)
+    print('Data downloaded and converted successfully!')
