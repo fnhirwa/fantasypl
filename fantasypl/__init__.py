7b2724c60e1212de4d71178979ce400f46006cf7
hirwa <hirwanshutiflx@gmail.com>
2024-02-13 23:58:32 +0530

    restructured the project

---
+++ fantasypl/__init__.py
@@ -0,0 +1,2 @@
+from fantasypl.parse_data import *
+from fantasypl.search import *
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+VERSION = "0.0.0"
+DESCRIPTION = "Fantasy Premier League API Wrapper"
+
+setup(
+    name="fantasypl",
+    version=VERSION,
+    author="Felix (Felix Hirwa Nshuti)",
+    author_email="<hirwanshutiflx@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fnhirwa/fantasypl",
+    long_description=open("README.md").read(),
+    keywords=["fantasy premier league", "fpl", "crawler"],
+    install_requires=[line for line in open("requirements.txt", "r", encoding="utf-8")],
+)
